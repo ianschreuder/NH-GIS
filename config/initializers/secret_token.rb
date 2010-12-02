cbf331fddc50a2e0dd909f4686c4e7756873b95d
Ian Schreuder <ian.schreuder@gmail.com>
2010-12-02 10:34:16 +0100

    First Rails Load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NhQis::Application.config.secret_token = '6e07d6b4624d72ffeb65ec3ce0d08691183425a99ca2b88b139fb5016bb10812bd65ea95d4af210f620b5234b417f7eab3de6974bd231d11e916a7b6fbfc9296'
